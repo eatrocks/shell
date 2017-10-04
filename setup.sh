758ddf009342fbd589fcccafbcb866d653baeba6
Bruce Campbell <eatrocks@gmail.com>
2017-10-04 14:50:52 -0600

    Cubs and git completion

--- setup.sh
+++ setup.sh
@@ -33,6 +33,12 @@ ln -s ~/shell/config/dot-extra ~/.extra
 /bin/rm ~/.path
 ln -s ~/shell/config/dot-path ~/.path
 
+# the dot-cubs file has passwords, see 1password for the contents
+if [ -f ~/shell/config/dot-cubs ]; then
+  /bin/rm ~/.cubs
+  ln -s ~/shell/config/dot-cubs ~/.cubs
+fi
+
 # set node permissions so sudo is not required (still use/need sudo for 'n')
 sudo chown -R $USER ~/.npm
 sudo chown -R $USER /usr/local/lib/node_modules
@@ -40,6 +46,24 @@ sudo chown -R $USER /usr/local/lib/node_modules
 # perm problems with npm install with -g
 sudo chgrp -R admin /usr/local
 sudo chmod -R g+w /usr/local
+
+# git auto-completion per https://git-scm.com/book/en/v1/Git-Basics-Tips-and-Tricks
+# git completion for all users requires bash 4 or higher... https://superuser.com/questions/31744/how-to-get-git-completion-bash-to-work-on-mac-os-x
+# note: I tried this "all users" method and had to upgrade bash to 4 and that broke my z config.  So I reverted.
+# So upgrade it... http://clubmate.fi/upgrade-to-bash-4-in-mac-os-x/
+# echo 'you need to manually upgrade bash to 4+ for bash completion to work (echo $BASH_VESION)'
+# echo '  see http://clubmate.fi/upgrade-to-bash-4-in-mac-os-x/'
+
+# this directory is where completion scripts go for all users, make sure it exists
+# AUTO_DIR=/opt/local/etc/bash_completion.d
+# if [ ! -d $AUTO_DIR ]; then
+#   sudo mkdir -p $AUTO_DIR
+# fi
+# if [ ! -f $AUTO_DIR/git-completion.bash ]; then
+#   curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ./bin/git-completion.bash && sudo mv ./bin/git-completion.bash $AUTO_DIR/git-completion.bash
+# fi
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/shell/bin/git-completion.bash.temp && mv ~/shell/bin/git-completion.bash.temp ~/shell/bin/git-completion.bash
+
 
 # 802.1X prompt when making network connection (un & )
 # un-check "Enable automatic connection" in
